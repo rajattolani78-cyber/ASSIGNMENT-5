59dbbbbca8f5413692a866cac92404425e2f4aca
rajattolani78-cyber <rajattolani78@gmail.com>
2025-10-04 01:12:16 +0530

    task_2 completed

---
+++ task_2.py
@@ -0,0 +1,6 @@
+original_list = list(range(1,11))
+print(f'original list:{original_list}')
+my_list_new = original_list[0:5]
+print(f'Extreted first five element:{my_list_new}')
+my_list_new.reverse()
+print("Reversed extered element:",my_list_new)
